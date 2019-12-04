a2dace5f86764affd1c8850c9ac5745121cf8d1e
nrico90 <nrico90@gmail.com>
2019-12-04 17:50:50 +0100

    data

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 4000;
 const teamRouter = require("./team/router");
 
 app.listen(port, () => console.log(`listening on :${port}`));
 
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 app.use(teamRouter);
--- team/router.js
+++ team/router.js
@@ -3,10 +3,16 @@ const { Router } = express;
 const Team = require("./model");
 
 const router = new Router();
+
 router.get("/team", (request, response, next) =>
   Team.findAll()
     .then(data => response.send(data))
     .catch(error => next(error))
 );
+router.post("/team", (request, response, next) => {
+  Team.create(request.body)
+    .then(team => response.send(team))
+    .catch(errors => next(erros));
+});
 
 module.exports = router;
